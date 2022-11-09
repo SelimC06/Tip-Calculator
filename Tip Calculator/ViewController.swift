46bdb31f7ab8aec76920e1eaaf56770e18d7f87e
SelimC06 <scoskunuzer@gmail.com>
2022-11-09 09:10:21 -0600

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Coskunuzer, Selim on 11/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
